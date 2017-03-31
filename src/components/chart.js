65cfe8d7853ebaa440a32b997e91e8e9789cf274
Jakub Syrek <jksyrek@gmail.com>
2017-03-31 14:27:41 +0200

    Add avarage temp + change units

--- src/components/chart.js
+++ src/components/chart.js
@@ -1,13 +1,19 @@
 import React from 'react'
-import { Sparklines, SparklinesLine, SparklinesBars } from 'react-sparklines'
+import _ from 'lodash'
+import { Sparklines, SparklinesLine, SparklinesBars, SparklinesReferenceLine } from 'react-sparklines'
 
+function avarage(data) {
+  return _.round(_.sum(data)/data.length)
+}
 
 export default (props) => {
   return (
     <div>
       <Sparklines height={120} width={180} data={props.data}>
         <SparklinesLine color={props.color} />
+        <SparklinesReferenceLine type="avg"/>
       </Sparklines>
+      <div>{ avarage(props.data) } { props.units }</div>
     </div>
   )
 }
