2edcdacf7e36dafa3bc5d7310cb13d6f0efdbf68
Jakub Syrek <jksyrek@gmail.com>
2017-04-04 14:59:46 +0200

    Implement post reducer

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -8,7 +8,7 @@ const INITIAL_STATE = {
 export default function (state = INITIAL_STATE, action) {
   switch(action.type) {
     case FETCH_POSTS:
-
+      return { ...state, all: action.payload.data}
     default:
       return state
   }
